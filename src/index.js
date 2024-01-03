ce3b5c5d45589b4726384c5a000ca36afd0a9117
faithchebet <faith.chebet@student.moringaschool.com>
2024-01-03 17:01:27 +0300

    src/index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
-document.addEventListener("DOMContentLoaded", () => {
+
   // your code here
-});
+
+  document.addEventListener('DOMContentLoaded', function () {
+    const form = document.getElementById('create-task-form');
+    const taskInput = document.getElementById('new-task-description');
+    const taskList = document.getElementById('tasks');
+    const submit =document.getElementById('delete-task')
+
+    form.addEventListener('submit', function (event) {
+      event.preventDefault(); 
+
+      
+      const taskDescription = taskInput.value;
+
+    
+      const newTask = document.createElement('li');
+      newTask.textContent = taskDescription;
+      taskList.appendChild(newTask);
+
+      const deleteTask = document.createElement('li');
+      deleteTask.textContent = taskDeletes;
+      taskList.appendChild(deleteTask);
+
+      
+      taskInput.value = '';
+    });
+  })
